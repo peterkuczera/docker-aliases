ae7bc43d2c48796b2d93a5c86896bd3b6c3212bd
Peter Kuczera <peter.kuczera@gmail.com>
2015-08-31 17:15:13 -0400

    Adding docker-connect

--- docker-aliases.sh
+++ docker-aliases.sh
@@ -7,3 +7,14 @@ docker-rm-stopped-containers() {
         echo "Nothing to delete"
     fi
 }
+
+docker-connect() {
+    if [[ $1 == "" ]]; then
+        echo "You must supply a container name"
+        return
+    fi
+
+    NAME=$1
+    ID=$(docker ps | grep $NAME | awk '{print $1}')
+    docker exec -it $ID bash -c "export TERM=linux; bash"
+}
